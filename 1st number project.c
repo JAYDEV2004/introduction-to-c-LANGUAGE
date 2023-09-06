a784c204e1d51a9e38dcf34f9bed50a8a47486db
JAYDEV2004 <144127232+JAYDEV2004@users.noreply.github.com>
2023-09-06 16:24:47 +0530

    Add files via upload

---
+++ 1st number project.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+main()
+
+{
+	printf("NAME   : SATANI JAYDEV SANJAYBHAI");
+	printf("\nAGE  : 18\n");
+	printf("SCHOOL : P.P.SAVANI INTERNATIONAL SCHOOL, ABRAMA.");
+}
---
+++ 3rd number pattern.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+main()
+
+{
+	printf("-\t-\t-");
+	printf("\n|\t\t|");
+	printf("\nJ\t\t|");
+	printf("\nA\t\t|");
+	printf("\nY\t\t|");
+	printf("\nD\t\t|");
+	printf("\nE\t\t|");
+	printf("\nV\t\t|");
+	printf("\n|\t\t|");
+	printf("\n-\t-\t-");
+}
